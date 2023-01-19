26df6bde11b5394c727356be89e4ed1a730de1d7
Manish Hyongoju <manish.hyongoju@tai.com.np>
2023-01-19 16:36:11 +0545

    fetching single data using id feature is added

--- Backend/controllers/ProductController.js
+++ Backend/controllers/ProductController.js
@@ -14,7 +14,7 @@ exports.newProducts = AsyncErrorHandler(async (req, res, next) => {
     })
 }
 )
-///
+///////////////////////Get all the products => api/v1/products ////
 exports.getProducts = AsyncErrorHandler(async (req, res, next) => {
 
     const product = await Product.find();
@@ -27,4 +27,24 @@ exports.getProducts = AsyncErrorHandler(async (req, res, next) => {
 
     })
 
+})
+
+//////////////////////Get sinigle product Details => api/v1/products////
+exports.getSingleProducts = AsyncErrorHandler(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return (
+            res.status(404).json({
+                success: false,
+                message: "Product not found",
+            })
+        )
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "Product successfully Fetched",
+        product,
+    })
 })
--- Backend/routes/product.js
+++ Backend/routes/product.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const { newProducts,
-    getProducts } = require('../controllers/ProductController');
+    getProducts,
+    getSingleProducts } = require('../controllers/ProductController');
 
 router.route('/products').get(getProducts);
+router.route('/product/:id').get(getSingleProducts);
 router.route('/product/new').post(newProducts);
 
 module.exports = router;
