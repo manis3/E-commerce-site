eeaaedd72a5cea3d56d496f72368f9bacce2fa33
Manish Hyongoju <manish.hyongoju@tai.com.np>
2023-01-25 14:44:52 +0545

    ...

--- Backend/utils/JWTToken.js
+++ Backend/utils/JWTToken.js
@@ -1,5 +1,12 @@
-// const sendToken = (user,statusCode,res =>{
-//     const token = user.getJwtToken();
-//     expires:new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000),
-//     httpOnly = true
-// })
+const sendToken = (user, statusCode, res => {
+    const token = user.getJwtToken();
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000),
+        httpOnly = true
+
+    res.status(201).json({
+        success: true,
+        token,
+        user
+    })
+})
+
